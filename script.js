3f62943b079ef4f0c09a840865f8e4a174ceada7
Geoffrey <geoffrpayton@gmail.com>
2021-06-02 18:09:26 -0400

    menu closes on click

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const navMenu = document.getElementById('navMenu');
 menuButton.addEventListener('click', () => {
   navMenu.classList.toggle('menu-visible');
 })
+
+navMenu.addEventListener('click', () => {
+  if (navMenu.classList.contains('menu-visible')) {
+    navMenu.classList.remove('menu-visible');
+  }
+})
