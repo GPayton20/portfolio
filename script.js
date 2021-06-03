eaaec609b69a110e00c2fa36e6c7e6b708ea317a
Geoffrey <geoffrpayton@gmail.com>
2021-06-03 18:52:48 -0400

    added function to trap focus inside hamburger menu while it is open

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const app = {};
 
+// Query elements from DOM
 app.menuButton = document.getElementById('menu-button');
 app.navMenu = document.getElementById('navMenu');
-
+// Query elements needed to trap focus inside menu when modal is open
+app.firstElement = app.menuButton;
+app.focusableElements = app.navMenu.querySelectorAll('[href]');
+app.lastElement = app.focusableElements[app.focusableElements.length - 1];
 
 app.init = () => {
   app.menuButton.addEventListener('click', () => {
     app.navMenu.classList.toggle('menu-visible');
+
+    if (app.navMenu.classList.contains('menu-visible')) {
+      document.addEventListener('keydown', app.trapFocus);
+    } else {
+      document.removeEventListener('keydown', app.trapFocus);
+    }
   });
   
   app.navMenu.addEventListener('click', () => {
@@ -16,6 +26,30 @@ app.init = () => {
   });
 
   app.buildIntersectionObserver();
+}
+
+// Function to trap focus inside hamburger menu so user cannot tab outside
+app.trapFocus = event => {
+  const tabPressed = event.key === 'Tab' || event.keyCode === 9;
+
+  // If tab key is not pressed, do nothing
+  if (!tabPressed) {
+    return;
+  }
+  // Check if shift key is pressed along with tab
+  if (event.shiftKey) {
+    // If focus is currently on the first focusable element (menu button), move focus to last focusable element (last link in menu)
+    if (document.activeElement === app.firstElement) {
+      app.lastElement.focus();
+      event.preventDefault();
+    }
+  } else {
+    // If focus is currently on last element and shift is not pressed, move focus back to first focusable element
+    if (document.activeElement === app.lastElement) {
+      app.firstElement.focus();
+      event.preventDefault();
+    }
+  }
 }
 
 // Method to allow skill icons to 'pop' in on scroll
