5e4c2e40f87ddbcedfa3a66da0fc1ae4caccfe8d
Geoffrey <geoffrpayton@gmail.com>
2021-06-03 17:16:17 -0400

    added intersection observer and pop-in animation for skills icons

--- script.js
+++ script.js
@@ -13,6 +13,36 @@ app.init = () => {
       app.navMenu.classList.remove('menu-visible');
     }
   });
+
+  app.buildIntersectionObserver();
+}
+
+app.buildIntersectionObserver = () => {
+  // Observer coversv viewport
+  app.options = {
+    root: null,
+    rootMargin: '0px 0px -50px 0px',
+    threshold: 1
+  }
+
+  app.observer = new IntersectionObserver(app.popIn, app.options);
+
+  app.skillsIcons = document.querySelectorAll('.section-skills .skills-list li');
+
+  app.skillsIcons.forEach(icon => {
+    app.observer.observe(icon);
+  })
+}
+
+app.popIn = entries => {
+  entries.forEach(entry => {
+    // Let skills icons "pop" into view as they pass observer threshold
+    if (entry.isIntersecting) {
+      // todo delete console log
+      console.log('Intersecting!');
+      entry.target.classList.add('pop-in');
+    }
+  })
 }
 
 app.init();
